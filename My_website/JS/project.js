d680473cc5288b063d65f9ed0945cbe5c9b6c689
Piyush raghav <141063566+Piyushraghav994@users.noreply.github.com>
2024-03-24 00:37:02 +0530

    Begining to backend

---
+++ My_website/JS/project.js
@@ -0,0 +1,37 @@
+//header section starts
+var menuBtn=document.querySelector('.navbar.menu-btn');
+var menuList=document.querySelector('.navbar.nav-list');
+var menuListItems=document.querySelector('.nav-list li a');
+
+menuBtn.addEventListener('click',function(){
+    menuBtn.classList.toggle('active');
+    menuList.classList.toggle('actie');
+});
+
+for(var i=0; i<menuListItems.clientHeight;i++){
+    menuListItems[i].addEventListener('click',menuItemsClicked);
+}
+
+function menuItemsClicked(){
+    menuBtn.classList.remove('avtive');
+    menuBtn.classList.remove('active');
+}
+
+// partners-slider section
+
+$('.partners-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
---
+++ My_website/index.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Project</title>
+</head>
+<!-- font awesome css link -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+<!-- main css file -->
+<link rel="stylesheet" href="CSS/project.css">
+<link rel="stylesheet" href="CSS/bootstrap.min.css">
+
+<!--font-generator-->
+<link rel="stylesheet" href="https://tools.picsart.com/text/font-generator/">
+<!-----font-style link-->
+<link rel="stylesheet" href="https://tools.picsart.com/text/font-generator/">
+
+<!-----link of owl carousel--------->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"/>
+
+
+
+
+<body>
+
+
+    <!------HEADER SECTION STARTS-->
+
+    <header class="header">
+        <!-- <img src="logo.png" alt=""> -->
+        <a href="index.php" class="logo"><i class="fa-solid fa-chalkboard-user"><br><br></i>𝕷𝖊𝖆𝖗𝖓𝖎𝖓𝖌 𝖜𝖎𝖙𝖍 𝖒𝖊!</a>
+
+        <nav class="navbar">
+            <a href="#home">
+                <underline></underline>Home
+            </a>
+            
+            <a href="#courses">
+                <underline>Courses</underline>
+            </a>
+
+            <a href="#My profile">
+                <underline>My profile</underline>
+            </a>
+
+            <a href="#Login">
+                <underline>Login</underline>
+            </a>
+            <a href="#signup">
+                <underline>Signup</underline>
+            </a>
+        
+            <a href="#review">
+                <underline>Review</underline>
+            </a>
+          
+            <a href="#review">
+                <underline>Logout</underline>
+            </a>
+          
+        </nav>
+        <a href="#" class="get-started-btn-container">
+            <button class="get-started-btn btn">Get Started!</button>
+        </a>
+        <div class="menu-btn">
+            <span></span>
+        </div>
+    </header> 
+    <!------header section ends-------->
+
+    <!----banner section starts-->
+    <section>
+        <div class="banner">
+            <div class="banner-desc">
+                <h2>"Elevate Your Education, Elevate Your Future"</h2>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo sequi, culpa laboriosam, id provident
+                    nulla exercitationem hic doloremque accusantium numquam amet fugit dignissimos nostrum magnam! Eos
+                    facilis atque totam Quo sequi, culpa laboriosam, id provident nulla exercitationem hic doloremque
+                    accusantium numquam amet fugit dignissimos nostrum magnam! Eos facilis atque </p>
+                <form class="search-bar">
+                    <!-- <input type="button" placeholder="Gets started"> -->
+                    <!-- <button >input type="button" placeholder=""></button> -->
+                    <!-- <i class="fa-solid fa-search"></i> -->
+                    <a href="" class="btn btn-danger">gets started </a>
+                </form>
+            </div>
+            <div class="banner-img">
+                <div class="banner-img-container">
+                    <img src="IMAGE_FOLDER/teacher3.jpg" alt="">
+                    <div class="states">
+                        <div class="total-courses">
+                            <div class="icon">
+                                <i class="fa-solid fa-book"></i>
+                            </div>
+                            <div class="desc">
+                                <span><bold>284+</bold></span>
+                                <span> <bold>Total Courses</bold></span>
+                            </div>
+                        </div>
+                        <div class="courses-ratings">
+                            <span>4.7 <i class="fa-solid fa-star"></i></span>
+                            <span><bold>Ratings (58K)</bold></span>
+
+                        </div>
+
+                    </div>
+                    <div class="pattern">
+                        <img src="IMAGE_FOLDER/dotted.png" alt="">
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+  
+    </section>
+
+<!----banner section ends-->
+
+<!----partners section starts-->
+
+<section class="partners">
+    <h3>Teachers can offer flexible scheduling to accommodate students' busy schedules.</h3>
+    <div class="owl-carousel owl-theme partners-slider">
+        <div class="item"><h4>1</h4></div>
+        <div class="item"><h4>2</h4></div>
+        <div class="item"><h4>3</h4></div>
+        <div class="item"><h4>4</h4></div>
+        <div class="item"><h4>5</h4></div>
+        <div class="item"><h4>6</h4></div>
+        <div class="item"><h4>7</h4></div>
+        <div class="item"><h4>8</h4></div>
+        <div class="item"><h4>9</h4></div>
+        <div class="item"><h4>10</h4></div>
+        <div class="item"><h4>11</h4></div>
+        <div class="item"><h4>12</h4></div>
+    </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<!----partners section ends-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!------link to JQuery---------------------------->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+<!-----link of owl carousel--------->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+
+<!-----------JAVASCRIPT LINK-------------->
+    <script  type="text/javascript" src="JS/project.js"></script>
+    <script  type="text/javascript" src="JS/bootstrap.min.js"></script>
+    <script  type="text/javascript" src="JS/jquery.min.js"></script>
+    <script  type="text/javascript" src="JS/popper.min.js"></script>
+</body>
+
+</html>
