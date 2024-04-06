6043151f87502514850d709cb06576e1ea1850b8
sarthak kumar <114852200+45Sarthak@users.noreply.github.com>
2024-04-06 23:05:32 +0530

    Admin connected

--- My_website/adminconnect.php
+++ My_website/adminconnect.php
@@ -1,16 +1,31 @@
-
-<!-- Connection of Admin login -->
 <?php
 
-    mysqli_connect("localhost","root","Piyushraghav","phpmyadmin");
+$User = $_POST['username'];
+$password = $_POST['password'];
 
-    if(mysqli_connect_error())
-    {
-    echo"Cannot connect";
-    }
-    else
-    {
-        echo"Connected";
-    }
+$conn = new mysqli('localhost', 'root', '', 'lms_db');
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }else{
+        // Prepared statement for insertion
+        $stmt = $conn->prepare("INSERT INTO admin_details(User_name , Passward) VALUES (?, ?)");
+        
+        // Check if statement preparation was successful
+        if ($stmt === false) {
+            die("Error in preparing statement: " . $conn->error);
+        }
+
+
+        $stmt->bind_param("ss", $User , $password);
+        if ($stmt->execute()) {
+            echo "Registration Successful!";
+        } else {
+            echo "Error: " . $stmt->error;
+        }
+
+         // Close the statement and database connection
+         $stmt->close();
+         $conn->close();
+     }
 
 ?>
