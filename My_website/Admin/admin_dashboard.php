8cdcb85999e6e9cffe5d1581f4edbf2cf846ecca
Piyush raghav <141063566+Piyushraghav994@users.noreply.github.com>
2024-04-18 01:28:40 +0530

    Admin dashboard started

--- My_website/Admin/admin_dashboard.php
+++ My_website/Admin/admin_dashboard.php
@@ -1,90 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    
-    <link rel="stylesheet" href="../CSS/bootstrap.min.css">
-
-    <link rel="stylesheet" href="../CSS/admindashboard.css">
-
-    <!-- Font Awesome CSS -->
-    <link rel="stylesheet" href="../CSS/all.min.css">
-    <!-- Google Font -->
-    <link href="https://fonts.googleapis.com/css?family=Ubuntu"
-    rel="stylesheet">
-
-</head>
-    <body>
-        <!-- Top Navbar-->
-        <nav class="navbar navbar-dark fixed-top p-0 shadow"
-        style="background-color: #225470;">
-       
-        <a class="navbar-brand col-sm-3 col-md-2 mr-0"
-        href="admindashboard.php">E-Learning <small class="text-white">Admin
-            Area</small></a>
-
-        </nav>
-
-        <!-- Side Bar-->
-        <div class="container-fluid mb-5" style="margin-top:40px;'
-            <div class="row">
-                <nav class="col-sm-3 col-md-2 bg-light sidebar py-5
-                d-print-none">
-                    <div class="sidebar-sticky">
-                        <ul class="nav flex-column">
-                            <li class="nav-item">
-                                <a class="nav-link" href="adminDashboard.php">
-                                <i class="fa fa-tachometer-alt"></i> Dashboard
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Courses
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Lessons
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Student 
-                                </a>
-                            </li><li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Sell report
-                                </a>
-                            </li><li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Payment status
-                                </a>
-                            </li><li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Feedback
-                                </a>
-                            </li><li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Change password 
-                                </a>
-                            </li><li class="nav-item">
-                                <a class="nav-link" href="#">
-                                <i class="fab fa-accessible-icon"></i>
-                                Logout
-                                </a>
-                            </li>
-                         </ul>
-                    </div>
-                </nav>
+<?php
+include('./adminheader.php');
+?>
                 <div class="col-sm-9 mt-5">
                     <div class="row mx-5 text-center">
                         <div class="col-sm-4 mt-5">
@@ -147,7 +63,7 @@
                                 <td>2000</td>
                                 <td><button type="submit" class="btn
                                 btn-secondary" name="delete" value="Delete"><i
-                                    class="far fa-trash-alt"> </i></button></td>
+                                    class="far fa-trash-alt"> </i>Delete</button></td>
                             </tr> 
                         </tbody>
                     </table>
@@ -161,11 +77,10 @@
         <!-- div Row close from header -->
          <!-- div Conatiner-fluid close from header -->
         <!-- Jquery and Boostrap JavaScript -->
-        <script type="text/javascript" src="JS/jquery.min.js"></script>
-        <script type="text/javascript" src="JS/popper.min.js"></script>
-        <script type="text/javascript" src="JS/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../JS/jquery.min.js"></script>
+        <script type="text/javascript" src="../JS/popper.min.js"></script>
+        <script type="text/javascript" src="../JS/bootstrap.min.js"></script>
         <!-- Font Awesome JS -->
-        <script type="text/javascript" src="JS/all.min.js"></script>           
-                        
-    </body>
+        <script type="text/javascript" src="../JS/all.min.js"></script>           
+         
 </html>
